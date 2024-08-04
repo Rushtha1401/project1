dd67c4c2d3d2ac4259a49cd402d34f93e7cddbe0
Rushtha1401 <727722euec145@skcet.ac.in>
2024-08-04 14:30:57 +0530

    Adding project file

---
+++ project.js
@@ -0,0 +1,42 @@
+const chatbody = document.querySelector(".chat-body");
+const textinput = document.querySelector("#txtip");
+const send = document.querySelector(".send");
+send.addEventListener("click",()=>renderUserMessage());
+textinput.addEventListener("keyup",(event)=>{   
+    if(event.keyCode===13){ //Pressing ENTER key to send
+        renderUserMessage();
+    }
+});
+const renderUserMessage=()=>{
+    const userInput=textinput.value;
+    renderMesEle(userInput,"user");
+    textinput.value="";  //to clear the message once we send it
+    setTimeout(()=>{
+        renderChatBotResponse(userInput);
+        setscrollPos();
+    },600);
+};
+const renderChatBotResponse=(userInput)=>{
+    const res=getChatBotResponse(userInput);
+    renderMesEle(res);
+};
+const renderMesEle=(txt,type)=>{
+    let className="user-message";
+    if(type!=='user'){
+        className="chatBotMessage";
+    }
+    const messageElement=document.createElement("div");
+    const txtNode=document.createTextNode(txt);
+    messageElement.classList.add(className);
+    messageElement.append(txtNode);
+    chatbody.append(messageElement);
+};
+const getChatBotResponse = (userInput)=>{
+    return responseObj[userInput.toLowerCase()] || "Please try something else";
+};
+const setscrollPos=()=>{
+    if(chatbody.scrollHeight>0){
+        chatbody.scrollTop = chatbody.scrollHeight; // Auto-scroll to the bottom
+
+    }
+}
